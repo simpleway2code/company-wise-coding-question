b908d6920ad65495b8cbe1da7f2abaff5a1f9001
garima1820 <67752635+garima1820@users.noreply.github.com>
2020-10-07 11:49:34 +0530

    added comments to code

--- Amazon/TrappingRainWater.java
+++ Amazon/TrappingRainWater.java
@@ -1,18 +1,25 @@
+/*we have given int array as size of bars and 
+we have to find that how much water it is capable to trap after raining
+*/
+
 public class TrappingRainWater {
     public static void main(String[] args) {
+     //height of each bars in form of int array
    int h[]={0,1,0,2,1,0,1,3,2,1,2,1};
    int res=trap(h);
         System.out.println(res); 
 }
+    //this method will give how much unit of water can be trapped
     public static int trap(int[] height) {
         
         if(height.length==0){
-            return 0;
+            return 0;    //if length of array is zero then simply we'll return 0
         }
         int s=height.length;
         int c=0;
-        int[] lmax=new int[s];
-        int[] rmax=new int[s];
+        int[] lmax=new int[s];  //making array in increasing order from left to right(max element till now)
+        int[] rmax=new int[s];  //making array in increasing order from right to left
+        
         lmax[0]=height[0];
         int l=height[0];
         rmax[s-1]=height[s-1];
@@ -34,6 +41,8 @@ public class TrappingRainWater {
                 rmax[j-1]=r;
             }
         }
+        
+        //comparing lmax and rmax
         for(int i=0;i<s;i++){
             if(lmax[i]<=rmax[i]){
                int f= height[i]-lmax[i];
@@ -50,7 +59,7 @@ public class TrappingRainWater {
                 c=c+f;
             }
         }
-        return c;
-    }
+        return c;   //returning result(unit of water that can be trapped)
+        }
     }    
 
