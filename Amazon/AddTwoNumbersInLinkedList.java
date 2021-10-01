071ba43ead8ac6a879003e9a7bbee596e01bfb12
Apple <apple@Apples-MacBook-Air.local>
2021-10-01 17:50:38 +0530

    adding comments

--- Amazon/AddTwoNumbersInLinkedList.java
+++ Amazon/AddTwoNumbersInLinkedList.java
@@ -41,15 +41,22 @@ public class AddTwoNumbersInLinkedList {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //initialize dummy node to keep on saving sum
         ListNode dummyHead = new ListNode(0);
         ListNode p = l1, q = l2, curr = dummyHead;
+        //variable to store carry for every summation
         int carry = 0;
+        //traversing both the linked list
         while (p != null || q != null) {
             int x = (p != null) ? p.val : 0;
             int y = (q != null) ? q.val : 0;
+            //taking sum
             int sum = carry + x + y;
+            //taking carry
             carry = sum / 10;
+            //creating node with the sum
             curr.next = new ListNode(sum % 10);
+            //moving forward
             curr = curr.next;
             if (p != null) p = p.next;
             if (q != null) q = q.next;
